60d26d4ab44718ce476b27258c15740d8b2badbd
athrvakulkarni11 <athrvakulkarni11@gmail.com>
2025-02-08 23:28:48 +0530

    test case passed

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,19 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    },
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': ['@swc/jest'],
-    },
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    moduleDirectories: ['node_modules', '<rootDir>/src'],
-  };
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['@swc/jest'],
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
--- src/app/components/MyComponent.test.js
+++ src/app/components/MyComponent.test.js
@@ -1,12 +1,11 @@
-// components/MyComponent.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MyComponent from './MyComponent';
-import '@testing-library/jest-dom/extend-expect';
 
 describe('MyComponent', () => {
   it('renders correctly', () => {
     render(<MyComponent />);
     expect(screen.getByText('Hello, World!')).toBeInTheDocument();
+    expect(screen.getByLabelText('Name')).toBeInTheDocument();
   });
 });
